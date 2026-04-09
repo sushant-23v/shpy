08e110a0c3771b26820cf34241c220743e55c123
Sushant Jadhav <sushant@23v.co>
2026-04-09 10:09:37 +0530

    Create src/Header.js — bioler plate

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Shpy Coffee</h1>
+            <nav>
+                <ul>
+                    <li><a href="#landing">Home</a></li>
+                    <li><a href="#menu">Menu</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;

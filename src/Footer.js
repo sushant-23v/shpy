6d4c6152c7302714d1b158e79968085bafd37229
Sushant Jadhav <sushant@23v.co>
2026-04-09 10:09:38 +0530

    Create src/Footer.js — bioler plate

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Contact us at: info@shpycoffee.com</p>
+            <p>Follow us on social media!</p>
+            <ul>
+                <li><a href="https://facebook.com/shpycoffee">Facebook</a></li>
+                <li><a href="https://instagram.com/shpycoffee">Instagram</a></li>
+                <li><a href="https://twitter.com/shpycoffee">Twitter</a></li>
+            </ul>
+        </footer>
+    );
+};
+
+export default Footer;

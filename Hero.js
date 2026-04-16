f83567197124678560a4d702d1a1d4b9356e41c8
Sushant Jadhav <sushant@23v.co>
2026-04-16 17:51:16 +0530

    Create Hero.js — added new files

---
+++ Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <section>
+            <h2>Welcome to Shopyfy</h2>
+            <p>Your one-stop shop for everything!</p>
+        </section>
+    );
+};
+
+export default Hero;
